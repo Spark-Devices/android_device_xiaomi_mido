7ece708bfefe4e89311f07b0f32b3cab7df30acd
RasyidAlKautsar <arkan1986muhammad@gmail.com>
2022-05-08 22:08:01 +0700

    mido: set target specific boot animation
    for avoid blank screen

--- spark_mido.mk
+++ spark_mido.mk
@@ -23,6 +23,7 @@ $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # Inherit some common SparkOS  stuff.
 $(call inherit-product, vendor/spark/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
